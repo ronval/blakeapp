4fc73701db920266cb173d52020d7853301121d2
ronval <ronald.m.valencia@gmail.com>
2019-07-31 20:32:00 +1000

    added prototype UI views for each of the required pages, added the controller for UI Pages and the routes for them

--- app/controllers/ui_pages_controller.rb
+++ app/controllers/ui_pages_controller.rb
@@ -2,8 +2,37 @@ class UiPagesController < ApplicationController
   layout :resolve_layout
   
   def index
+  end
+
+  def add_class
     
   end
+
+  def add_teacher
+    
+  end
+
+  def new_lesson
+    
+  end
+
+  def edit_lesson
+    
+  end
+
+  def add_student
+    
+  end
+
+  def student_list
+    
+  end
+
+  def grade_lesson
+    
+  end
+
+
   
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'pages#home'
   get "ui_pages" => "ui_pages#index"
+  get "ui_add_class" => "ui_pages#add_class", :as => :ui_add_class
+  get "ui_add_teacher" => "ui_pages#add_teacher", :as => :ui_add_teacher
+  get "ui_new_lesson" => "ui_pages#new_lesson", :as => :ui_create_lesson
+  get "ui_edit_lesson" => "ui_pages#edit_lesson", :as => :ui_edit_lesson
+  get "ui_add_student" => "ui_pages#add_student", :as => :ui_add_student
+  get "ui_student_list" => "ui_pages#student_list", :as => :ui_student_index
+  get "ui_grade_lesson" => "ui_pages#grade_lesson", :as=> :ui_grade_lesson
 
 end
